e863eaf030d6931a48482ce578e0fc65d1ddb19e
Dmytro Pavlenko <atosdo@live.com>
2014-12-19 16:46:51 +0200

    *

---
+++ TourOperator.Domain.DataAccessLayer/Abstract/IRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TourOperator.Domain.DataAccessLayer.Abstract
+{
+    public interface IRepository<TEntity>
+    {
+        IEnumerable<TEntity> Get(Expression<Func<TEntity, bool>> filter = null,
+            Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null,
+            string includeProperties = "");
+
+        TEntity Find(object id);
+        void Insert(TEntity entity);
+        void Delete(object id);
+        void Delete(TEntity entity);
+        void Update(TEntity entity);
+    }
+}
---
+++ TourOperator.Domain.DataAccessLayer/Repositories/GenericRepository.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using TourOperator.Domain.DataAccessLayer.Abstract;
+
+namespace TourOperator.Domain.DataAccessLayer.Repositories
+{
+    public class GenericRepository<TEntiry> : IRepository<TEntiry>
+    {
+        private readonly DbContext _context;
+
+        public GenericRepository(DbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<TEntiry> Get(Expression<Func<TEntiry, bool>> filter = null, Func<IQueryable<TEntiry>, IOrderedQueryable<TEntiry>> orderBy = null, string includeProperties = "")
+        {
+            throw new NotImplementedException();
+        }
+
+        public TEntiry Find(object id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(TEntiry entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(object id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(TEntiry entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(TEntiry entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
