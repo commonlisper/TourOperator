ce04abcb0661fffed419dc1298eb7497ae96e019
Dmytro Pavlenko <atosdo@live.com>
2014-12-19 14:35:03 +0200

    *

---
+++ TourOperator.Web/Controllers/AdminController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TourOperator.Web.Controllers
+{
+    [Authorize]
+    public class AdminController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Countries()
+        {
+            return View();
+        }
+
+        public ActionResult Tours()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ActionResult HealthResorts()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ActionResult Hotels()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
