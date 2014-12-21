b5df7ae5e50d423709d18c3f0da0984dafc763ea
Dmytro Pavlenko <atosdo@live.com>
2014-12-21 23:15:41 +0200

    *

---
+++ TourOperator.Web/Helpers/ActiveMenuLinkHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Html;
+
+namespace TourOperator.Web.Helpers
+{
+    public static class ActiveMenuLinkHelper
+    {
+        public static string MenuActive(
+            this HtmlHelper htmlHelper,
+            string actionName,
+            string controllerName)
+        {
+            string currentAction = htmlHelper.ViewContext.RouteData.GetRequiredString("action");
+            string currentController = htmlHelper.ViewContext.RouteData.GetRequiredString("controller");
+            const string className = "active";
+
+            if (actionName == currentAction && controllerName == currentController)
+            {
+                return className;
+            }
+
+            return null;
+        }
+    }
+}
