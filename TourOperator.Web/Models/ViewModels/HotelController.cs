6e998be127fa1ed01d691e698bef18f790fb34a3
Dmytro Pavlenko <atosdo@live.com>
2014-12-29 18:27:43 +0200

    *

---
+++ TourOperator.Web/Models/ViewModels/HotelController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TourOperator.Domain.DataAccessLayer;
+using TourOperator.Domain.DataAccessLayer.Abstract;
+
+namespace TourOperator.Web.Models.ViewModels
+{
+    public class HotelController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork = new UnitOfWork(new DomainDbContext());
+
+        public ActionResult Details(Guid id)
+        {
+            return View(_unitOfWork.HotelRepository.Find(id));
+        }
+    }
+}
