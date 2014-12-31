9c0c2c8ea3671a84ddb6882e625d846cc0b063f9
Dmytro Pavlenko <atosdo@live.com>
2014-12-31 12:11:20 +0200

    *

--- TourOperator.Web/App_Start/BundleConfig.cs
+++ TourOperator.Web/App_Start/BundleConfig.cs
@@ -1,4 +1,5 @@
-﻿using System.Web;
+﻿using System.Threading;
+using System.Web;
 using System.Web.Optimization;
 
 namespace TourOperator.Web
@@ -8,31 +9,37 @@ namespace TourOperator.Web
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+            bundles.UseCdn = true;
+
+            // Scripts
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                "~/Scripts/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+                "~/Scripts/jquery.validate*"));
 
             bundles.Add(new ScriptBundle("~/bundles/jquery-ui").Include(
-                        "~/Scripts/jquery-ui-{version}.js"));
+                "~/Scripts/jquery-ui-{version}.js"));
 
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+                "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                "~/Scripts/bootstrap.js",
+                "~/Scripts/respond.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/tinymce").Include(
-                      "~/3rdPartyComponents/tinymce/tinymce.min.js"));
+                "~/3rdPartyComponents/tinymce/tinymce.min.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/globalize").Include(
+                "~/Scripts/globalize/globalize.js",
+                "~/Scripts/globalize/cultures/globalize.culture." + Thread.CurrentThread.CurrentCulture.Name + ".js"));
+
+            // Styles
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css",
-                      "~/Content/themes/base/all.css"));
+                "~/Content/bootstrap.css",
+                "~/Content/site.css",
+                "~/Content/themes/base/all.css"));
         }
     }
 }
